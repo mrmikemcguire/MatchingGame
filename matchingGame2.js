266e89cffd9e9869ac35adcc32c7f05e30c16a09
mrmikemcguire <moonshotmike@gmail.com>
2019-11-05 17:31:44 -0700

    lesson 7 finished

--- matchingGame2.js
+++ matchingGame2.js
@@ -6,19 +6,18 @@ var gameWidth = 2;
 
 createGrid(gameHeight, gameWidth);
 function createGrid (gameHeight, gameWidth) {
-    for (var row = 0; row < gameHeight; row++)
-    {
-    for (var col = 0; col < gameWidth; col++)
-        {
-        createCard(0, row, col);
+    for (var row = 0; row < gameHeight; row++) {
+        for (var col = 0; col < gameWidth; col++) {
+            createCard(0, row, col);
         }
     }
 }
 
-
 function createCard(cardNum, posX, posY) {
     var card = document.createElement('img');
     card.src = 'matchCards/card' + cardNum + '.png';
+    // card.src = 'matchCards/cardback.png';
+    card.num = cardNum;
     card.style.position = 'absolute';
     card.style.left = posX * (cardSize + cardSpacing) + cardSpacing + 'px';
     card.style.top = posY * (cardSize + cardSpacing) + cardSpacing +'px';
